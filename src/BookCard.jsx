6d81738db0492aa1a31139bf20898c2ca8139159
vineetkhemnani <vineetkhemnani09@gmail.com>
2024-06-09 15:51:53 +0530

    responsive card changes

--- src/BookCard.jsx
+++ src/BookCard.jsx
@@ -6,8 +6,8 @@ const BookCard = ({ title, author, coverId, notShelf, onAddToShelf }) => {
     : 'https://via.placeholder.com/150'
 
   return (
-    <div className="max-w-xs rounded overflow-hidden shadow-lg bg-slate-100 px-14 py-4">
-      <img className="w-60 h-64" src={coverUrl} alt={title} />
+    <div className="max-w-xs w-full rounded overflow-hidden shadow-lg bg-slate-200 px-10 py-4 max-sm:px-5">
+      <img className="xs:w-60 h-64" src={coverUrl} alt={title} />
       <div className="py-4">
         <div className="font-bold text-lg mb-1">{title}</div>
         <p className="text-gray-700 text-base mb-1">{author}</p>
--- src/ShimmerCard.jsx
+++ src/ShimmerCard.jsx
@@ -1,8 +1,8 @@
 const ShimmerCard = () => {
   return (
     <>
-      <div className="max-w-xs w-full rounded overflow-hidden shadow-lg bg-slate-200 animate-pulse px-10 py-4">
-        <div className="w-60 h-64 bg-gray-300"></div>
+      <div className="max-w-xs w-full rounded overflow-hidden shadow-lg bg-slate-200 animate-pulse px-10 py-4 max-sm:px-5">
+        <div className="md:w-60 h-64 bg-gray-300"></div>
         <div className="py-4">
           <div className="h-4 bg-gray-300 mb-1"></div>
           <div className="h-4 bg-gray-300 mb-1"></div>
